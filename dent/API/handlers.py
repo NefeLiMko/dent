134e1de048e2e1d7b5e89c3d7a6bb7ec95db87fb
NefeLiMko <capitalofbelarus@gmail.com>
2024-06-01 19:57:02 +0300

    fix json loading

--- dent/API/handlers.py
+++ dent/API/handlers.py
@@ -3,19 +3,22 @@ import dent.settings as settings
 import pandas as pd
 import requests
 import json
-
+from parsers.models import ProductModel
 def xls_handler(fs):
     filename = f"out_{fs.split('/')[-1].split('.')[0]}.json"
+    
     xls = pd.read_excel(f"{settings.BASE_DIR}{fs}") # use r before absolute file path 
     out = {}
     values = xls[xls.columns[1]].values
     for value in values:
         if str(value)!="nan":
-            print(value)
-            url = f'http://127.0.0.1:8000/search/{value}'
             try:
-                r = requests.get(url)
-                out[value] = r.json()
+                for element in ProductModel.objects.filter(articul=value):
+                    if value in out.keys():
+                        out[value].append({"title":element.name, "price": element.price, "site":element.site, "date":str(element.date)})
+                    else:
+                        out[value] = [{"articul":element.articul,"title":element.name, "price": element.price, "site":element.site, "date":str(element.date)}]
+                    print(out[value])
             except:
                 pass
     try:
@@ -26,5 +29,5 @@ def xls_handler(fs):
     full_filename = os.path.join(settings.MEDIA_ROOT, "data", filename)
     with open(full_filename, 'w+', encoding='utf8') as out_json:
         json.dump(out, out_json, indent=4, ensure_ascii=False)
-    
+    print(f"{settings.BASE_DIR}{fs}")
     return filename.split("out_data_")[-1].split(".json")[0]
